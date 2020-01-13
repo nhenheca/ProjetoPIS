afbd91eefb9aa231656cf62de8eb67b47ccb3f0d
nhenheca <33757448+nhenheca@users.noreply.github.com>
2020-01-13 10:27:11 +0000

    Registo e Login Fufando

--- PyRacoes/pyracoes_main/urls.py
+++ PyRacoes/pyracoes_main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index),
     path("signup/", views.signup, name="signup"),
     path("login/", views.login),
-    path("home/", views.home)
+    path("home/", views.home, name="home"),
+    path("logout/", views.logout, name="logout")
 ]
--- PyRacoes/pyracoes_main/views.py
+++ PyRacoes/pyracoes_main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import authenticate
+from django.contrib.auth import logout as auth_logout
 from .models import User, Animal, Ration
 from django.http import HttpResponse
 
@@ -21,16 +22,28 @@ def signup(request):
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             auth_login(request, user)
-            return redirect(home)
+            return redirect("home")
     return render(request, 'signup.html', {'form': form})
     # return render(request, "signup.html")
 
 
 def login(request):
-    return render(request, "login.html")
+    password = request.POST.get('password')
+    username = request.POST.get('username')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        auth_login(request, user)
+        return redirect("home")
+    else:
+        return render(request, "login.html")
 
 
 def home(request):
     rations = Ration.objects.all()
     args = {"rations": rations}
     return render(request, "home.html", args)
+
+
+def logout(request):
+    auth_logout(request)
+    return render(request, "index.html")
