16f8982a76f06e0cabad28f712573cb87a67e08b
nhenheca <33757448+nhenheca@users.noreply.github.com>
2020-01-17 00:40:06 +0000

    Update views.py

--- PyRacoes/pyracoes_main/views.py
+++ PyRacoes/pyracoes_main/views.py
@@ -61,7 +61,7 @@ def search(request):
         ration_port = request.POST['ration_port']
         ration_classification = request.POST['ration_classification']
         #aux = Ration.objects.all().get(ration_age=ration_age, ration_atrib=ration_atrib, ration_type=ration_type, ration_port=ration_port, ration_classification=ration_classification)
-        aux = Ration.objects.all().filter(ration_age__age_name=age).filter(ration_atrib__attributes_name=atrib).filter(ration_type__type_name=type).filter(ration_port__port_name=port).filter(ration_classification__classification_name=classification)
+        aux = Ration.objects.all().filter(ration_age__age_name=ration_age).filter(ration_atrib__attributes_name=ration_atrib).filter(ration_type__type_name=ration_type).filter(ration_port__port_name=ration_port).filter(ration_classification__classification_name=ration_classification)
         #aux = Ration.objects.all()
         args = {"aux": aux}
         return render(request, "home.html", args)
